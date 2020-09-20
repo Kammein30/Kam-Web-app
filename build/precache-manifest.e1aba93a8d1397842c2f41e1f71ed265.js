52db26b865a8643cf55cda1fa495b13ef47ca19e
Kam <mitchellk@msoe.edu>
2020-09-20 17:46:35 -0500

    something to try

---
+++ build/precache-manifest.e1aba93a8d1397842c2f41e1f71ed265.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922063988a7c75384380eb60a4b9d9ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e86c1c3add0e689fae1",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f85587633dfe89178a8e",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6e86c1c3add0e689fae1",
+    "url": "/static/js/2.e6550e22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e6550e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85587633dfe89178a8e",
+    "url": "/static/js/main.80dfd0b5.chunk.js"
+  },
+  {
+    "revision": "da821e3e17146a67b854",
+    "url": "/static/js/runtime-main.2c0deb60.js"
+  },
+  {
+    "revision": "0725890653b2c29552f2138c0677f5e5",
+    "url": "/static/media/MSOE_Headshots_2653.07258906.jpg"
+  }
+]);
