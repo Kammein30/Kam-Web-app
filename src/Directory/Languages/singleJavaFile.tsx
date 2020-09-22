301b54d8be5ec0956c769b83216fdf76dbc65f20
Kam <mitchellk@msoe.edu>
2020-09-22 18:41:58 -0500

    Finally got the different colors per project to work!

---
+++ src/Directory/Languages/singleJavaFile.tsx
@@ -0,0 +1,42 @@
+import React, {Component, useState} from 'react';
+import CSS from "csstype";
+let key = 1;
+
+type myProps = {
+    id: number,
+    title : string,
+    description: string,
+    details : string[]
+}
+
+
+class SingleJavaFile extends Component<myProps> {
+    constructor(props: myProps) {
+        super(props);
+    };
+
+    styling: CSS.Properties = {
+        backgroundColor: this.props.id % 2 === 0 ? 'lightgreen' : 'lightblue'
+    };
+
+    getDetail() {
+        return this.props.details.map((detail)=> (
+          <li key={key++}>{detail}</li>
+        ));
+    };
+
+    render() {
+
+        return(
+            <div style={this.styling}>
+                <h3>{this.props.title}</h3>
+                <p>{this.props.description}</p>
+                <ul>
+                    {this.getDetail()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default SingleJavaFile;
