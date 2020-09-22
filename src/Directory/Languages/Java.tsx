40dd62ad3293b929782f7eee92d67adff62682e1
Kam <mitchellk@msoe.edu>
2020-09-22 18:46:16 -0500

    Deleting unneeded stuff and changing variable names

--- src/Directory/Languages/Java.tsx
+++ src/Directory/Languages/Java.tsx
@@ -1,19 +1,18 @@
 import React, {Component, CSSProperties} from 'react';
-//Use the filesystem to load the test file
 import SingleJavaFile from "./singleJavaFile";
 import "bootstrap/dist/css/bootstrap.css";
 
 
 const javaFile = require ("../../Project Info/Java.json");
-let key = 1;
+let changeColor = 1;
 
 class Java extends Component {
 
 
     render() {
-        return <div style={{backgroundColor: key % 2 === 0 ? 'lightblue' : 'lightgreen'}}>
+        return <div style={{backgroundColor: changeColor % 2 === 0 ? 'lightblue' : 'lightgreen'}}>
                  {javaFile.map((file: typeof javaFile)=>(
-                    <SingleJavaFile key={key++}  id={key} title={file.title} description={file.description} details={file.details}/>
+                    <SingleJavaFile key={changeColor++}  id={changeColor} title={file.title} description={file.description} details={file.details}/>
                 ))}
             </div>;
     }
--- src/Directory/Languages/singleJavaFile.tsx
+++ src/Directory/Languages/singleJavaFile.tsx
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import CSS from "csstype";
 let key = 1;
 
